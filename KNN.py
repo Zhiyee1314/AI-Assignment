4707701fdd856fd8c79c4d5b1f101141e66f56a2
EJ-1017 <eejie1017@gmail.com>
2026-07-14 23:31:15 +0800

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,5 +1,11 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 df = pd.read_csv("diabetes.csv")
 
@@ -8,3 +14,19 @@ columns = ['Glucose','BloodPressure','SkinThickness','Insulin','BMI']
 df[columns] = df[columns].replace(0,np.nan)
 
 df.fillna(df.median(), inplace=True)
+
+X = df.drop("Outcome", axis=1)
+y = df["Outcome"]
+
+scaler = StandardScaler()
+
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+prediction = knn.predict(X_test)
+accuracy = accuracy_score(y_test, prediction)
+
+print(accuracy)
+print(classification_report(y_test, prediction))
